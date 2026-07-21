ceb471baac8ee5abb6a2744b6ca430b2c0564f2a
Roberto Serrano <rserranog@gmail.com>
2026-07-21 18:24:20 +0200

    Complete Module 1 - Session 7: Loops (while and for)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-console.log(true == 1);
-
-console.log(true === 1);
-
-console.log(false == 0);
-
-console.log(false === 0);
+for (let i = 10; i >= 1; i--) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
